94343877121168ecc59fd272e22f3851ffcbff08
PhanNhan123 <50936169+PhanNhan123@users.noreply.github.com>
2020-06-05 23:21:30 +0700

    context

---
+++ DoAnFW/DoAnFW/Models/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+namespace DoAnFW.Models
+{
+    public class StoreContext
+    {
+        public string ConnectionString { get; set; }
+        public StoreContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+        private MySqlConnection GetConnection() //lấy connection 
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+} 
