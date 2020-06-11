adfd8facc3221e5868eb13e4272da08c38d07248
Luong <17520728@gm.uit.edu.vn>
2020-06-11 13:36:41 +0700

    add more models

---
+++ DoAnFW/DoAnFW/Models/NguoiDung.cs
@@ -0,0 +1,46 @@
+﻿using Org.BouncyCastle.Crypto.Tls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace DoAnFW.Models
+{
+    public class NguoiDung
+    {
+        private int ma;
+        private string ten;
+        private string taiKhoan;
+        private string matKhau;
+        public NguoiDung()
+        { }
+        public NguoiDung(int maND, string tenND, string TK, string MK)
+        {
+            ma = maND;
+            ten = tenND;
+            taiKhoan = TK;
+            matKhau = MK;
+        }
+        public int Ma 
+        {
+            get{ return ma; }
+            set { ma = value; }
+        }
+        public string Ten
+        {
+            get { return ten; }
+            set { ten= value;  }
+        }
+        public string TaiKhoan
+        {
+            get { return taiKhoan; }
+            set { taiKhoan=value;  }
+        }
+            public string MatKhau 
+        {
+            get { return matKhau; }
+            set { matKhau= value ;  }
+        }
+    }
+}
---
+++ DoAnFW/DoAnFW/Models/NhanHieu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoAnFW.Models
+{
+    public class NhanHieu
+    {
+        private string maNH;
+        private string tenNH;
+        private string noiSX;
+        public NhanHieu() { }
+        public NhanHieu(string ma, string ten, string NoiSX)
+        {
+            maNH = ma;
+            tenNH = ten;
+            noiSX = NoiSX;
+        }
+        public string MaNH
+        {
+            get { return maNH; }
+            set { maNH = value; }
+        }
+        public string TenNH
+        {
+            get { return tenNH; }
+            set { tenNH = value; }
+        }
+        public string NoiSX
+        {
+            get { return noiSX; }
+            set { noiSX = value; }
+        }
+    }
+}
