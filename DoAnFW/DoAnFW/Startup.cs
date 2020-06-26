b28ec85dc9de3ae010f75c0d58cf9afe8051ced4
Luong <17520728@gm.uit.edu.vn>
2020-06-26 14:57:58 +0700

    cm

--- DoAnFW/DoAnFW/Startup.cs
+++ DoAnFW/DoAnFW/Startup.cs
@@ -49,6 +49,9 @@ namespace DoAnFW
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                      name: "Admin",
+                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
